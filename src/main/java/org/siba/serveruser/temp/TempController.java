a1d605aeceba9c4215e31a15f9b41af1c82bb988
Coco0719 <ejsvk3284@gmail.com>
2020-08-25 04:03:59 +0900

    Initialize spring boot project and add temp api

---
+++ src/main/java/org/siba/serveruser/temp/TempController.java
@@ -0,0 +1,16 @@
+package org.siba.serveruser.temp;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TempController {
+
+    @GetMapping("/temp")
+    public ResponseEntity returnTempApi() {
+        return ResponseEntity.ok(new Object() {
+            public String temp = "jenkins";
+        });
+    }
+}
