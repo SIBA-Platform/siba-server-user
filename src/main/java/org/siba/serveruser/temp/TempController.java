248e6d67a4f0cccabc63ffb71483b276b7dbaa01
Gyojun An <Sencom1028@gmail.com>
2020-09-04 05:41:04 +0900

    jenkins webhook function test

--- src/main/java/org/siba/serveruser/temp/TempController.java
+++ src/main/java/org/siba/serveruser/temp/TempController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TempController {
 
+    //Test Url
     @GetMapping("/temp")
     public ResponseEntity returnTempApi() {
         return ResponseEntity.ok(new Object() {
